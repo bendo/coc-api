3985b6296e86b1614997791bddf58eac69c516e3
bendo <zdeno.osina@gmail.com>
2016-02-14 00:48:57 +0100

    more info in member list

--- src/components/MemberInfo.js
+++ src/components/MemberInfo.js
@@ -8,7 +8,18 @@ export default class MemberInfo extends Component {
 
         return (
             <div className="member">
-                {member.get('name')}
+                <div><h2>{member.get('clanRank')}</h2></div>
+                <div><img src={member.getIn(['leagueBadgeImg', 's']).split(',')[0]}/></div>
+                <div><strong>{member.get('expLevel')}</strong></div>
+                <div className="name">
+                    {member.get('name')}<br/>
+                    <span className="xs">{member.get('role')}</span>
+                </div>
+                <div className="info">
+                    <div><span className="xs">Troops donated:</span><br/>{member.get('donations')}</div>
+                    <div><span className="xs">Troops received:</span><br/>{member.get('donationsReceived')}</div>
+                    <div><span className="xs">Trophies:</span><br/>{member.get('trophies')}</div>
+                </div>
             </div>
         );
     }
